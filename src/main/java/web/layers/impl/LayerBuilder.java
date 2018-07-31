4fd5a2eb95b05a2a5ec93301c874833a06fa6149
rakovpublic <rakovpublic@gmail.com>
2018-07-31 10:01:11 +0300

    Layer builder mock added

---
+++ src/main/java/web/layers/impl/LayerBuilder.java
@@ -0,0 +1,23 @@
+package web.layers.impl;
+
+import web.storages.IInputMeta;
+import web.storages.ILayersMeta;
+
+//TODO:add implementation
+public class LayerBuilder {
+    private ILayersMeta layersMeta;
+
+    public LayerBuilder(ILayersMeta layersMeta) {
+        this.layersMeta = layersMeta;
+    }
+
+    public LayerBuilder withLayerId(int layerId){
+        return this;
+    }
+    public LayerBuilder withInput(IInputMeta meta){
+        return this;
+    }
+    public LayerBuilder withNeuronRange(int start, int end){
+        return  this;
+    }
+}
