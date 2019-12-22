d3cea0888d553aae78dda22efab16e23619c6638
rakovpublic <rakovpublic@gmail.com>
2019-12-22 11:37:42 +0200

    Finished local runner

--- worker/src/main/java/application/LocalApplication.java
+++ worker/src/main/java/application/LocalApplication.java
@@ -13,22 +13,26 @@ import net.storages.structimpl.StructMeta;
 import synchronizer.IContext;
 import synchronizer.utils.InstantiationUtils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
 import java.sql.Struct;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class LocalApplication implements IApplication {
     @Override
     public void startApplication(IContext context) {
-        String inputType =context.getProperty("configuration.input.type");
-        String inputPath =context.getProperty("configuration.input.path");
+        String inputType = context.getProperty("configuration.input.type");
+        String inputPath = context.getProperty("configuration.input.path");
         StructBuilder structBuilder = new StructBuilder();
-        String layerPath=context.getProperty("configuration.input.layermeta");
+        String layerPath = context.getProperty("configuration.input.layermeta");
 
-        if(inputType.equals("fileSystem")){
-            String fileSystemClass =context.getProperty("configuration.filesystem.class");
+        if (inputType.equals("fileSystem")) {
+            String fileSystemClass = context.getProperty("configuration.filesystem.class");
             Class<IFileSystem> clazz = null;
             try {
-                clazz = (Class<IFileSystem>)Class.forName(fileSystemClass);
+                clazz = (Class<IFileSystem>) Class.forName(fileSystemClass);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
                 //TODO:add logger
@@ -36,24 +40,24 @@ public class LocalApplication implements IApplication {
             }
             String fileSystemConstructorArgs = context.getProperty("configuration.filesystem.constructor.args");
             String fileSystemConstructorArgsType = context.getProperty("configuration.filesystem.constructor.args.types");
-            IFileSystem fs= InstantiationUtils.<IFileSystem>getObject(clazz,getObjects(fileSystemConstructorArgs),getTypes(fileSystemConstructorArgsType));
-            String serializers= context.getProperty("configuration.serializers");
-            IInputMeta inputMeta=new FileInputMeta(fs.getItem(inputPath),getSerializers(serializers),fs);
+            IFileSystem fs = InstantiationUtils.<IFileSystem>getObject(clazz, getObjects(fileSystemConstructorArgs), getTypes(fileSystemConstructorArgsType));
+            String serializers = context.getProperty("configuration.serializers");
+            IInputMeta inputMeta = new FileInputMeta(fs.getItem(inputPath), getSerializers(serializers), fs);
             structBuilder.withInitInputMeta(inputMeta);
             structBuilder.withHiddenInputMeta(inputMeta);
-            structBuilder.withLayersMeta(new FileLayersMeta<>(fs.getItem(layerPath),fs));
-            StructMeta meta= structBuilder.build();
-            int i=0;
-            for(ILayerMeta met:meta.getLayers()){
-                LayerBuilder lb= new LayerBuilder();
+            structBuilder.withLayersMeta(new FileLayersMeta<>(fs.getItem(layerPath), fs));
+            StructMeta meta = structBuilder.build();
+            int i = 0;
+            for (ILayerMeta met : meta.getLayers()) {
+                LayerBuilder lb = new LayerBuilder();
                 lb.withLayer(met);
                 lb.withInput(meta.getInputs(i));
-                ILayer layer=lb.build();
-                if(layer.validateGlobal()&&layer.validateLocal()){
+                ILayer layer = lb.build();
+                if (layer.validateGlobal() && layer.validateLocal()) {
                     //TODO: add logger invalid layer configuration and exception
                 }
                 layer.process();
-                while(!layer.isProcessed()){
+                while (!layer.isProcessed()) {
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
@@ -67,24 +71,55 @@ public class LocalApplication implements IApplication {
             }
 
 
-
-        }else {
+        } else {
 
         }
 
     }
 
-    private Class<?> []  getTypes(String str){
-        return null;
-        //TODO:Add implementation
+    private Class<?>[] getTypes(String str) {
+        List<Class<?>> reuslt = new ArrayList<>();
+        try {
+            if (str.contains(":")) {
+                String[] parts = str.split(":");
+                for (String cl : parts) {
+                    reuslt.add(Class.forName(cl));
+                }
+            } else {
+                reuslt.add(Class.forName(str));
+            }
+        } catch (ClassNotFoundException e) {
+            //TODO:Add logger
+        }
+        return (Class<?>[]) reuslt.toArray();
+
     }
-    private Object [] getObjects(String str){
-        return null;
-        //TODO:Add implementation
+
+    private Object[] getObjects(String str) {
+        Object[] obj=null;
+        try {
+            byte b[] = str.getBytes();
+            ByteArrayInputStream bi = new ByteArrayInputStream(b);
+            ObjectInputStream si = new ObjectInputStream(bi);
+           obj = (Object[]) si.readObject();
+        }catch (Exception ex){
+            //TODO:Add logger
+        }
+        return obj;
+
     }
-    private HashMap<Class<? extends ISignal>, ISerializer<? extends ISignal, String>> getSerializers(String str){
-        return null;
-        //TODO:Add implementation
+
+    private HashMap<Class<? extends ISignal>, ISerializer<? extends ISignal, String>> getSerializers(String str) {
+        HashMap<Class<? extends ISignal>, ISerializer<? extends ISignal, String>> obj=null;
+        try {
+            byte b[] = str.getBytes();
+            ByteArrayInputStream bi = new ByteArrayInputStream(b);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            obj = (HashMap<Class<? extends ISignal>, ISerializer<? extends ISignal, String>>) si.readObject();
+        }catch (Exception ex){
+            //TODO:Add logger
+        }
+        return obj;
     }
 
 
