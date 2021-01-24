5f3a0bea0001614249ac8d0b7a967fc7b36c3f88
dmytro.raskovskyi <dmytro.raskovskyi@nordstrom.com>
2021-01-24 23:02:46 +0200

    phase 2 interface development

--- master/jneuronnetmaster/src/main/java/services/ILayerService.java
+++ master/jneuronnetmaster/src/main/java/services/ILayerService.java
@@ -1,4 +1,9 @@
 package services;
 
+import java.util.List;
+
 public interface ILayerService {
+    void deleteNeuron(Integer layerId, Long neuronId);
+    void addNeuron(String neuronJson,Integer layerId);
+    void mergeNeurons(Integer layerId, List<Long> neuronIds);
 }
--- master/jneuronnetmaster/src/main/java/services/ISignalsService.java
+++ master/jneuronnetmaster/src/main/java/services/ISignalsService.java
@@ -1,4 +1,9 @@
 package services;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface ISignalsService {
+    void saveSignal(String signalJson, Integer layerId, Long neuronId);
+    List<String> loadSignals(HashMap<Integer,List<Long>> targets);
 }
