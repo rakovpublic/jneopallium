6cc59e4fa318ce804f00521eb1cfee539d876eb3
DmytroRakovskyi <rakovpublic@gmail.com>
2021-01-31 23:25:32 +0200

    phase 2 interface development #2

---
+++ master/jneuronnetmaster/src/main/java/model/ISignalLayer.java
@@ -0,0 +1,4 @@
+package model;
+
+public interface ISignalLayer extends DataStorage {
+}
--- master/jneuronnetmaster/src/main/java/services/ILayerService.java
+++ master/jneuronnetmaster/src/main/java/services/ILayerService.java
@@ -1,9 +1,16 @@
 package services;
 
+import model.INeuronLayer;
+import model.ISignalLayer;
+
 import java.util.List;
 
 public interface ILayerService {
     void deleteNeuron(Integer layerId, Long neuronId);
     void addNeuron(String neuronJson,Integer layerId);
     void mergeNeurons(Integer layerId, List<Long> neuronIds);
+    void isProcessed(Long layerId);
+    <K extends INeuronLayer> K getNeuronLayerForNode(Integer nodeId) ;
+    <K extends ISignalLayer> K getSignalLayerForNode(Integer nodeId,Integer layerId);
+
 }
