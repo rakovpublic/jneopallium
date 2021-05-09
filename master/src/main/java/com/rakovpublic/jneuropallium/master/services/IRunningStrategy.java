4316f4a528c756b4375e7dba9b96a9101bc58e92
DmytroRakovskyi <rakovpublic@gmail.com>
2021-05-09 16:04:31 +0300

    Class naming refactoring

--- master/src/main/java/com/rakovpublic/jneuropallium/master/services/IRunningStrategy.java
+++ master/src/main/java/com/rakovpublic/jneuropallium/master/services/IRunningStrategy.java
@@ -4,5 +4,5 @@ import com.rakovpublic.jneuropallium.worker.net.storages.ILayersMeta;
 
 //interface for cycle running before next run with next input
 public interface IRunningStrategy {
-    ISignalsPersistStorage populateInput(ISignalsPersistStorage signalsPersistStorage, ILayersMeta layersMeta);
+    void populateInput(ISignalsPersistStorage signalsPersistStorage, ILayersMeta layersMeta);
 }
