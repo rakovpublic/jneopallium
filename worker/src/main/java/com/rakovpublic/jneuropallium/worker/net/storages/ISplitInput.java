1967d90751b84371fc358512c1847a86d2f32912
DmytroRakovskyi <rakovpublic@gmail.com>
2021-05-30 15:14:01 +0300

    added abstract http input meta

--- worker/src/main/java/com/rakovpublic/jneuropallium/worker/net/storages/ISplitInput.java
+++ worker/src/main/java/com/rakovpublic/jneuropallium/worker/net/storages/ISplitInput.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 public interface ISplitInput extends IStorageMeta {
     HashMap<Long, List<ISignal>> readInputs();
-    List<ISignal> readInputsForNeuron(Long neuronId);
     void saveResults(HashMap<Long, List<ISignal>> signals);
     void setNodeIdentifier(String name);
     ISplitInput getNewInstance();
     List<? extends INeuron> getNeurons();
+    void setCycleInputsMap(HashMap<String,Long> neuronInputNameMapping);
+    HashMap<String, Long>  getCycleInputsMap();
+    Integer getCurrentLoopCount();
 }
---
+++ worker/src/main/java/com/rakovpublic/jneuropallium/worker/net/storages/http/AbstractHttpInputMeta.java
@@ -0,0 +1,146 @@
+package com.rakovpublic.jneuropallium.worker.net.storages.http;
+
+
+import com.rakovpublic.jneuropallium.worker.net.signals.ISignal;
+import com.rakovpublic.jneuropallium.worker.net.storages.ISplitInput;
+import com.rakovpublic.jneuropallium.worker.neuron.INeuron;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+
+public abstract class AbstractHttpInputMeta implements ISplitInput {
+    private String nodeId;
+    private String readInputsEndpoint;
+    private String readNeuronsEndpoint;
+    private String sendResultEndpoint;
+
+    private HashMap<String, Long> neuronInputNameMapping;
+    private Integer currentInnerLoopCount;
+
+    public AbstractHttpInputMeta(String nodeId, String readInputsEndpoint, String readNeuronsEndpoint, String sendResultEndpoint, HashMap<String, Long> neuronInputNameMapping, Integer currentInnerLoopCount) {
+        this.nodeId = nodeId;
+        this.readInputsEndpoint = readInputsEndpoint;
+        this.readNeuronsEndpoint = readNeuronsEndpoint;
+        this.sendResultEndpoint = sendResultEndpoint;
+        this.neuronInputNameMapping = neuronInputNameMapping;
+        this.currentInnerLoopCount = currentInnerLoopCount;
+    }
+
+    @Override
+    public HashMap<Long, List<ISignal>> readInputs() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(readNeuronsEndpoint))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .authenticator(Authenticator.getDefault())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO: add logger
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            //TODO: add logger
+        }
+
+        return parseSignalsForNeurons(response);
+    }
+
+
+    @Override
+    public void saveResults(HashMap<Long, List<ISignal>> signals) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(readNeuronsEndpoint))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(resultToJSON(signals)))
+                .build();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .authenticator(Authenticator.getDefault())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO: add logger
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            //TODO: add logger
+        }
+    }
+
+    @Override
+    public void setNodeIdentifier(String name) {
+        nodeId=name;
+    }
+
+
+    @Override
+    public List<? extends INeuron> getNeurons() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(readNeuronsEndpoint))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .authenticator(Authenticator.getDefault())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO: add logger
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            //TODO: add logger
+        }
+
+        return parseNeurons(response);
+    }
+
+    @Override
+    public void setCycleInputsMap(HashMap<String, Long> neuronInputNameMapping) {
+        this.neuronInputNameMapping=neuronInputNameMapping;
+    }
+
+    @Override
+    public HashMap<String, Long> getCycleInputsMap() {
+        return neuronInputNameMapping;
+    }
+
+    @Override
+    public Integer getCurrentLoopCount() {
+        return currentInnerLoopCount;
+    }
+
+
+    protected abstract HashMap<Long, List<ISignal>> parseSignalsForNeurons(HttpResponse<String> response);
+
+    protected abstract List<? extends INeuron> parseNeurons(HttpResponse<String> response);
+
+    protected abstract String resultToJSON(HashMap<Long, List<ISignal>> signals);
+}
