c15210b62d26131fa7a34cec02e44cb4a4c7dc22
DmytroRakovskyi <rakovpublic@gmail.com>
2021-06-02 18:20:28 +0300

    added master signal storage service interface

---
+++ master/src/main/java/com/rakovpublic/jneuropallium/master/services/ISignalHistoryService.java
@@ -0,0 +1,11 @@
+package com.rakovpublic.jneuropallium.master.services;
+
+import com.rakovpublic.jneuropallium.worker.net.signals.ISignal;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface ISignalHistoryService {
+    void saveSignals(Integer run, Integer layerId, HashMap<Long, List<ISignal>> signals);
+    List<ISignal> loadSignals(Integer run, Integer layerId,Long neuronId);
+}
