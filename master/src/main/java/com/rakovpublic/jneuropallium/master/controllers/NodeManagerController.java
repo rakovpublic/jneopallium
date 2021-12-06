598d33b4c7ab1453394951865ad4babe2a2ea4ac
DmytroRakovskyi <rakovpublic@gmail.com>
2021-12-06 20:08:54 +0200

    Node manager controller done

--- master/src/main/java/com/rakovpublic/jneuropallium/master/controllers/NodeManagerController.java
+++ master/src/main/java/com/rakovpublic/jneuropallium/master/controllers/NodeManagerController.java
@@ -1,12 +1,14 @@
 package com.rakovpublic.jneuropallium.master.controllers;
 
 import com.rakovpublic.jneuropallium.master.model.NodeCompleteRequest;
+import com.rakovpublic.jneuropallium.master.model.SplitInputResponse;
 import com.rakovpublic.jneuropallium.master.services.IInputService;
 import com.rakovpublic.jneuropallium.master.services.MasterContext;
 import com.rakovpublic.jneuropallium.master.services.impl.NodeManager;
 import com.rakovpublic.jneuropallium.master.services.impl.NodeStatus;
 import com.rakovpublic.jneuropallium.worker.net.storages.ISplitInput;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,20 +22,34 @@ public class NodeManagerController {
 
     private IInputService inputService;
     @PostMapping("/completeRun")
-    public void nodeComplete(@RequestBody NodeCompleteRequest request){
-        nodeManager.setNodeStatus(request.getNodeName(), NodeStatus.IDLE);
-        //TODO: add response
+    public ResponseEntity<?> nodeComplete(@RequestBody NodeCompleteRequest request){
+        try {
+            nodeManager.setNodeStatus(request.getNodeName(), NodeStatus.IDLE);
+
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e);
+        }
+        return ResponseEntity.ok().build();
     }
     @PostMapping("/nextRun")
-    public void getNextRun(@RequestBody NodeCompleteRequest request){
-      ISplitInput splitInput = inputService.getNext(request.getNodeName());
-      nodeManager.setNodeStatus(request.getNodeName(),NodeStatus.RUNNING);
-        //TODO: add response
+    public ResponseEntity<?> getNextRun(@RequestBody NodeCompleteRequest request){
+        ISplitInput splitInput = null;
+        try {
+            splitInput = inputService.getNext(request.getNodeName());
+            nodeManager.setNodeStatus(request.getNodeName(),NodeStatus.RUNNING);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e);
+        }
+        return ResponseEntity.ok(new SplitInputResponse(splitInput));
     }
     @PostMapping("/register")
-    public void registerNode(@RequestBody NodeCompleteRequest request){
+    public ResponseEntity<?> registerNode(@RequestBody NodeCompleteRequest request){
+        try{
         nodeManager.register(request.getNodeName());
-        //TODO: add response
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e);
+        }
+        return ResponseEntity.ok().build();
     }
 
 }
---
+++ master/src/main/java/com/rakovpublic/jneuropallium/master/model/SplitInputResponse.java
@@ -0,0 +1,29 @@
+package com.rakovpublic.jneuropallium.master.model;
+
+import com.rakovpublic.jneuropallium.worker.net.storages.ISplitInput;
+
+public class SplitInputResponse {
+    private String className;
+    private ISplitInput splitInput;
+
+    public SplitInputResponse(ISplitInput splitInput) {
+        this.splitInput = splitInput;
+        this.className = splitInput.getClass().getCanonicalName();
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public ISplitInput getSplitInput() {
+        return splitInput;
+    }
+
+    public void setSplitInput(ISplitInput splitInput) {
+        this.splitInput = splitInput;
+    }
+}
