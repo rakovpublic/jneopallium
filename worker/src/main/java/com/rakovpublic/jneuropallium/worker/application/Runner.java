6d60b85fbf51b94ba1cd5ad4cb21f7138403aa62
dmytro <dmytro.rakovskyi@reality.co>
2023-08-29 13:11:05 +0300

    small refactoring

--- worker/src/main/java/com/rakovpublic/jneuropallium/worker/application/Runner.java
+++ worker/src/main/java/com/rakovpublic/jneuropallium/worker/application/Runner.java
@@ -2,12 +2,15 @@ package com.rakovpublic.jneuropallium.worker.application;
 
 import com.rakovpublic.jneuropallium.worker.synchronizer.IContext;
 import com.rakovpublic.jneuropallium.worker.util.JarClassLoaderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public abstract class Runner implements IRunner {
     private IContext context;
+    private final static Logger logger = LogManager.getLogger(Runner.class);
 
     public Runner() {
         context = getContext();
@@ -19,15 +22,17 @@ public abstract class Runner implements IRunner {
         try {
             uris = new URL[]{new URL(jarPath)};
         } catch (MalformedURLException e) {
-            //TODO: add logger
+            logger.error("Incorrect url",e);
             throw new RuntimeException(e);
         }
         JarClassLoaderService classLoaderService = new JarClassLoaderService(uris);
         IApplication application;
         if (mode.equals("local")) {
             application = new LocalApplication();
-        } else {
+        } else if (mode.equals("http")) {
             application = new HttpClusterApplication();
+        }else{
+            application = new GRPCBasedApplication();
         }
         application.startApplication(context, classLoaderService);
     }
