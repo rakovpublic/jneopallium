7d7cc476f1b8c6f3a3ce751074b902143f7eb08d
rakovpublic <rakovpublic@gmail.com>
2023-10-31 22:04:25 +0200

    Started preparations for discriminators adding

---
+++ worker/src/main/java/com/rakovpublic/jneuropallium/worker/net/layers/DiscriminatorResultLayer.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023. Rakovskyi Dmytro
+ */
+
+package com.rakovpublic.jneuropallium.worker.net.layers;
+
+public interface DiscriminatorResultLayer extends IResultLayer{
+    boolean hasPass();
+}
