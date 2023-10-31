ae850de1a8c8b6af177b873030a93fc2b0179abf
rakovpublic <rakovpublic@gmail.com>
2023-10-31 22:00:12 +0200

    Started preparations for discriminators adding

---
+++ worker/src/main/java/com/rakovpublic/jneuropallium/worker/net/storages/inmemory/InMemoryDiscriminatorSourceSignals.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023. Rakovskyi Dmytro
+ */
+
+package com.rakovpublic.jneuropallium.worker.net.storages.inmemory;
+
+import com.rakovpublic.jneuropallium.worker.net.signals.IInputSignal;
+import com.rakovpublic.jneuropallium.worker.net.signals.IResultSignal;
+import com.rakovpublic.jneuropallium.worker.net.storages.IInitInput;
+import com.rakovpublic.jneuropallium.worker.net.storages.INeuronNetInput;
+
+import java.util.HashMap;
+import java.util.List;
+
+//TODO: implement
+public class InMemoryDiscriminatorSourceSignals implements IInitInput {
+    @Override
+    public List<IInputSignal> readSignals() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public INeuronNetInput getNeuronNetInput() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, List<IResultSignal>> getDesiredResults() {
+        return null;
+    }
+}
