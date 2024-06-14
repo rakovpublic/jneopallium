b076bb3fe0b1b5bd72584f243d688a57cfb143d2
dmytro <dmytro.rakovskyi@reality.co>
2024-06-14 13:30:34 +0300

    added result output implementation

--- worker/src/main/java/com/rakovpublic/jneuropallium/worker/test/definitions/ioutils/TestOutputAggregator.java
+++ worker/src/main/java/com/rakovpublic/jneuropallium/worker/test/definitions/ioutils/TestOutputAggregator.java
@@ -4,16 +4,34 @@
 
 package com.rakovpublic.jneuropallium.worker.test.definitions.ioutils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rakovpublic.jneuropallium.worker.application.IOutputAggregator;
 import com.rakovpublic.jneuropallium.worker.net.layers.IResult;
 import com.rakovpublic.jneuropallium.worker.util.IContext;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class TestOutputAggregator implements IOutputAggregator {
     @Override
     public void save(List<IResult> results, long timestamp, long run, IContext context) {
+        ObjectMapper mapper = new ObjectMapper();
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter("F:\\git\\res\\"+run+"\\"+timestamp+"\\result"));
+            writer.write(mapper.writeValueAsString(results));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
 
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
